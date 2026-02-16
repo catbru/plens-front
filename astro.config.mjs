860b60f492e0521e071fb111674240ff869c8a72
catbru <catbru@gmail.com>
2026-02-16 23:40:11 +0100

    Configure production URL for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  // site: 'https://teu-usuari.github.io',
-  // base: '/plens-front',
+    site: 'https://catbru.github.io',
+    base: '/plens-front',
 });
